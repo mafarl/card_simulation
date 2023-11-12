e328d26f4b582db14087278dbaa37fd2d570b413
Ashley Card <ashleyxcard@gmail.com>
2023-11-12 13:31:30 +0000

    abc

--- CardGame.java
+++ CardGame.java
@@ -10,35 +10,38 @@ public class CardGame{
 
 	public static void main(String[] args) {
 		
-		String numPlayers;
-		// Check no players
-		while (true){
-			Scanner myObj = new Scanner(System.in);
-			System.out.println("Enter number of players: ");
-			numPlayers = myObj.nextLine();
+		int numPlayers = Helper.playerAmount();
 		
-			try {
-				int number = Integer.parseInt(numPlayers);
-				if (number < 1){
-					throw new NumberFormatException();
-				}
-				break;
-			} catch (NumberFormatException e) {
-				System.out.println("Invalid player number input");
+		File packFile = Helper.getPack(numPlayers);
+
+		// Read cards into an array
+		ArrayList<Card> initialPack =  new ArrayList<Card>();
+		try {
+			Scanner myReader = new Scanner(packFile);
+			while (myReader.hasNextLine()) {
+				String data2 = myReader.nextLine();
+				int data3 = Integer.parseInt(data2);
+				initialPack.add(new Card(data3));
 			}
+		} catch (FileNotFoundException e) {
+			  System.out.println("Incorrect file name/path");
+		}
+
+
+
+		// Give cards to players
+		int counter = 0;
+		while(counter < 4*numPlayers){
+			allPlayers.get(counter%numPlayers).addCard(initialPack.get(counter));
+			counter++;
 		}
 		
-		// Validate the file + pack
-		while (true){
-			Scanner myObj = new Scanner(System.in);
-			System.out.println("Enter file path: ");
-			String fileName = myObj.nextLine();
-			
-			boolean validity = Helper.checkPack(fileName, Integer.parseInt(numPlayers));
-			if (validity == true){
-				break;
-			}
+		// Give cards to decks
+		int counter2 = 4*numPlayers;
+		while(counter2 < 8*numPlayers){
+			allCardDecks.get(counter2%numPlayers).addCard(initialPack.get(counter2));
+			counter2++;
 		}
+		
 	}
-	
 }
--- Helper.java
+++ Helper.java
@@ -3,38 +3,81 @@ import java.io.*;
 
 public class Helper{
 
-	public static boolean checkPack(String fileName, int n){
-		try {
-			  File packFile = new File(fileName);
+	    public static int playerAmount(){
+		// Check no players
+		while (true){
+			try {
+                Scanner myObj = new Scanner(System.in);
+                System.out.println("Enter number of players: ");
+                int numPlayers = myObj.nextInt();
+				if (numPlayers >= 1){
+                	return(numPlayers);
+                    
+				}
+				System.out.println("Enter an integer greater than 0");
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid player number input");
+			}
+		}
+    }
+
+	public static File getPack(int playerAmount){
+		while (true){
+			try{
+				// Getting filename
+				Scanner myObj2 = new Scanner(System.in);
+				System.out.println("Enter file path");
+				String fileName = myObj2.nextLine();
+				File packFile = new File(fileName); 
+
+				if (!packFile.exists()){
+					throw new FileNotFoundException();
+				}
+				
+				// Check if the pack is valid
+				if (Helper.checkPack(packFile, playerAmount)){
+					System.out.println("Card pack loaded successfully");
+					return packFile;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid card face value, non-integer found");
+			} catch (FileNotFoundException e){
+				System.out.println("Incorrect file name/path");
+			} catch (NumberFormatException e){
+				System.out.println("Invalid card face value, integer less than 1 found");
+			}
+			
+		}
+	}
+
+	public static boolean checkPack(File packFile, int n) throws FileNotFoundException {
+		
 			  Scanner myReader = new Scanner(packFile);
 			  int line_counter = 0;
 			  while (myReader.hasNextLine()) {
-					String data = myReader.nextLine();
-					line_counter ++;
-					// Verify if each row is non negative int
-					try {
-						int number = Integer.parseInt(data);
-						if (number < 1){
-							throw new NumberFormatException();
-						}
-					} catch (NumberFormatException e) {
-						System.out.println("Invalid card's face value");
+
+					// Check for whitespace
+					if (myReader.nextLine() == ""){
+						System.out.println("Whitespace in file, invalid pack");
 						return false;
 					}
+
+					int number = myReader.nextInt();
+					line_counter ++;
+
+					// Verify if each row is non negative int
+					if (number < 1){
+						throw new NumberFormatException();
+					}
 			  }
-			  
+
 			  // Verify no rows
 			  if (line_counter != 8*n){
-			  	  System.out.println("Incorrect number of cards");
+			  	  System.out.println("Incorrect number of cards in pack");
 			  	  return false;
 			  }
 			  
 			  myReader.close();
 			  return true;
-			  
-		} catch (FileNotFoundException e) {
-			  System.out.println("Incorrect file name/path");
-			  return false;
-		}
 	}
 }
