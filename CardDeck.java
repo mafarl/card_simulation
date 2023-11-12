496923ec387c8048b6efb149256dede87650319b
mafarl <108734570+mafarl@users.noreply.github.com>
2023-11-12 13:39:52 +0000

    Update CardDeck.java

--- CardDeck.java
+++ CardDeck.java
@@ -4,15 +4,34 @@
 // Distribute cards within players (array for each Player eg. player 1: [card1,card3,card5,card7])
 // Create n CardDecks
 // Distribute cards within CardDecks (array for each CardDeck eg. carddeck 1: [card10,card13,card15,card17])
+import java.util.*;
+import java.io.*;
+
 public class CardDeck{
 
 		// Attributes
+		private ArrayList<Card> cards = new ArrayList<Card>();
+		private int deckIndex;
 		
-		// Methods
-
+		// Methods	
+		public ArrayList<Card> getDeck(){
+			return cards;
+		}
+		
+		public void addCard(Card card){
+			cards.add(card);
+		}
+		
+		// Add card at the top of the deck (index 0 in cards)
+		public void addTopCard(){}
+		
+		// Remove card from the bottom of the deck (index -1 in cards)
+		public void removeCard(){}
+		
 		// Constructor
-		public CardDeck(){
-
+		public CardDeck(int deckIndex){
+			this.deckIndex = deckIndex;
+			this.cards = cards;
 		}
 
 }
